f14eb55ea6d1f405ec3df1bbf56d83709b5c1167
Kelly He <kellyhe@mit.edu>
2022-04-06 18:47:53 -0400

    add flask server backend (not connected to model yet)

---
+++ flask-api/server.py
@@ -0,0 +1,46 @@
+# Using flask to make an api
+# import necessary libraries and functions
+from flask import Flask, jsonify, request
+  
+# creating a Flask app
+app = Flask(__name__)
+  
+# on the terminal type: curl http://127.0.0.1:5000/
+# returns hello world when we use GET.
+# returns the data that we send when we use POST.
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+    if(request.method == 'GET'):
+  
+        data = "hello world get"
+        return jsonify({'data': data})
+
+    if request.method == 'POST':
+        # average_time = request.form.get('average_time')
+        # choices = request.form.get('choices')
+        # created_by = request.form.get('created_by')
+        # difficulty_level = request.form.get('difficulty_level')
+        # question = request.form.get('question')
+        # topics = request.form.get('topics')
+        print(request.form, request.args)
+        # request.args gets from params
+        img = request.form.get("img") # form = body
+        print(img)
+        data = "hello world posts"
+        return jsonify({'data': img})
+  
+  
+# # A simple function to calculate the square of a number
+# # the number to be squared is sent in the URL when we use GET
+# # on the terminal type: curl http://127.0.0.1:5000 / home / 10
+# # this returns 100 (square of 10)
+# @app.route('/home/<int:num>', methods = ['GET'])
+# def disp(num):
+  
+#     return jsonify({'data': num**2})
+  
+  
+# driver function
+if __name__ == '__main__':
+  
+    app.run(debug = True)
