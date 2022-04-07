6450464dea2349637f3baa88b346e99735de16cc
itzdonaldxd <donaldl@mit.edu>
2022-04-06 20:02:39 -0400

    Workin on connecting the model and the server

--- flask-api/server.py
+++ flask-api/server.py
@@ -1,10 +1,22 @@
 # Using flask to make an api
 # import necessary libraries and functions
 from flask import Flask, jsonify, request
-  
+
+import skimage
+from skimage.transform import resize
+
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import load_model
+
 # creating a Flask app
 app = Flask(__name__)
   
+imageSize = 64
+asl_model = load_model("model_1.h5")
+letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','del','nothing','space', 'undefined']
+
 # on the terminal type: curl http://127.0.0.1:5000/
 # returns hello world when we use GET.
 # returns the data that we send when we use POST.
@@ -22,12 +34,24 @@ def home():
         # difficulty_level = request.form.get('difficulty_level')
         # question = request.form.get('question')
         # topics = request.form.get('topics')
-        print(request.form, request.args)
+        # print(request.form, request.args)
         # request.args gets from params
         img = request.form.get("img") # form = body
-        print(img)
+        img_list = np.array(eval(img))
+        print("HELLO")
+        img_file = skimage.transform.resize(img_list, (imageSize, imageSize, 3))
+        print(img_file)
+        img_arr = np.asarray(img_file).reshape((-1, imageSize, imageSize, 3))
+        # print(img_arr)
+
+
+        
+        prediction = asl_model.predict(img_arr).argmax(axis=-1)[0]
+        print(prediction)
+        letter = letters[prediction]
+
         data = "hello world posts"
-        return jsonify({'data': img})
+        return jsonify({'data': letter})
   
   
 # # A simple function to calculate the square of a number
