f3f6d18b93cfb5c930fe99d8210786e56da9cb68
Kelly He <kellyhe@mit.edu>
2022-04-20 21:57:25 -0400

    speech command instruction and feedback

--- ttth-ui/src/components/SpeechDetection.js
+++ ttth-ui/src/components/SpeechDetection.js
@@ -4,20 +4,29 @@ import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognitio
 
 export default function SpeechDetection(props) {
   const [guess, setGuess] = useState('')
+
   const commands = [
     {
       command: [
       'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
-      callback: ({command}) => props.onCheckGuess(command),
+      callback: ({command}) => {
+        resetTranscript()
+        props.onCheckGuess(command)
+      }
     },
     {
       command: "the letter *",
-      callback: (letter) => props.onCheckGuess(letter)
+      callback: (letter) => {
+        resetTranscript()
+        props.onCheckGuess(letter)
+      }
+
     },
     {
       command: ["skip"],
       callback: () => {
         if (!props.won && !props.showAnswer) {
+          resetTranscript()
           props.onNext();
         } else {
           console.log("can't skip");
@@ -28,6 +37,7 @@ export default function SpeechDetection(props) {
       command: ["next"],
       callback: () => {
         if (props.won || props.showAnswer) {
+          resetTranscript()
           props.onNext();
         } else {
           console.log("can't go next");
@@ -39,6 +49,7 @@ export default function SpeechDetection(props) {
       callback: () => {
         if (!props.won && !props.showAnswer) {
           props.onViewAnswer()
+          resetTranscript()
         } else {
           console.log("can't view answer");
         }
@@ -47,6 +58,7 @@ export default function SpeechDetection(props) {
     {
       command: ["hint", "show hint", "hide hint"],
       callback: () => {
+        resetTranscript();
         props.toggleHint();
       }
     }
@@ -78,8 +90,15 @@ export default function SpeechDetection(props) {
 
   return (
     <div className='row'>
-      <Button onClick={resetTranscript} variant="outlined" size="small">Reset Mic</Button>
-      <p className='line-height-dense'>Microphone: <b>{listening ? 'ON' : 'OFF'}</b></p>
+      {/* <Button onClick={resetTranscript} variant="outlined" size="small">Reset Mic</Button> */}
+      <p className='line-height-dense'><b>Try Speech Commands!</b></p>
+      <div>
+        <p className='line-height-dense'>- The letter "x"</p>
+        <p className='line-height-dense'>- skip</p>
+        <p className='line-height-dense'>- next</p>
+        <p className='line-height-dense'>- show answer</p>
+        <p className='line-height-dense'>- hint</p>
+      </div>
       <p className='line-height-dense'>{`"${transcript}"`}</p>
       {/* <button onClick={SpeechRecognition.startListening}>Start</button>
       <button onClick={SpeechRecognition.stopListening}>Stop</button> */}
